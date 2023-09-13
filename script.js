9b2ac5b6580ebbea5ad3003bcaa752d3a7ff26b1
DimitriKwihangana <kwihanganadimitri@gmail.com>
2023-09-13 14:51:20 +0200

    added Do or C sound

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-  
+  let audio = new Audio("tunes/a.wav");
+
+  const playTune = (key) => {
+    audio.play(key);
+  };
   pianoKeys.forEach((key) => {
     const dataKey = key.getAttribute("data-key");
-    console.log(dataKey);
+    // console.log(dataKey);
+    key.addEventListener("click", () => playTune(dataKey));
   });
 });
+
